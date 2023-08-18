0899c6e2648051cdf250cd4740bcbc61bae7ca33
Jakub Kubecka <kubeckaj@s96n41.grendel.cscaa.dk>
2023-08-18 09:26:06 +0200

    update

---
+++ database_v2/Full_database/script.sh
@@ -0,0 +1,12 @@
+#sbatch --mem=40gb JKsend sh script.sh
+
+
+#find ../Articles/ -type f -name database.pkl > all_databases.txt; find ../Articles/ -type f -name database_s*.pkl >> all_databases.txt
+#rm full_database.pkl 2>/dev/null
+#JKQC `cat all_databases.txt | xargs` -out full_database.pkl
+
+#SPLIT DATABASE IF NEEDED + MAKE STRUCTURES.xyz
+length=`JKQC full_database.pkl -b | wc -l`
+splits=`echo "($length-$length%10000)/10000+1" | bc`
+JKQC full_database.pkl -split $splits -out full_database.pkl
+
